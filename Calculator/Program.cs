5ef963b98e41da98085bc27d72f0829637f5c2f4
Gabriel Khachatryan <gmkhach@gmail.com>
2018-12-02 23:31:58 -0500

    the most simple case is implemented

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,45 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter a math expression\n\n>>> ");
+                    string input = Console.ReadLine().Trim();
+                    Console.WriteLine("\nEquals to: " + Calculator(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static double Calculator(string input)
+        {
+            double num1 = input[0] - 48.00;
+            double num2 = input[2] - 48.00;
+            double result = 0;
+            switch (input[1])
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            return result;
         }
     }
 }
